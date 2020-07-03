8d39707317be89474f26c74cc8e4d045f66e1dc9
VetalSh <jedvemya86@gmail.com>
2020-07-03 13:42:13 +0300

    Lesson 19

--- script/script.js
+++ script/script.js
@@ -41,12 +41,76 @@ window.addEventListener('DOMContentLoaded', function() {
         timerHours.textContent = '00';
         timerMinutes.textContent = '00';
         timerSeconds.textContent = '00';
+        clearInterval(idInterval);
       }
     }
-
     updateClock();
   }
+  let idInterval = setInterval(countTimer, 1000, '22 july 2020');
 
-  setInterval(countTimer, 1000, '04 july 2020');
+  // Menu
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+          menu = document.querySelector('menu'),
+          closeBtn = document.querySelector('.close-btn'),
+          menuItems = menu.querySelectorAll('ul>li');
+
+    const handlerMenu = () => {
+      // if (!menu.style.transform || menu.style.transform === `translate(-100%)`){
+      //   menu.style.transform = `translate(0)`;
+      // } else {
+      //   menu.style.transform = `translate(-100%)`;
+      // }
+      menu.classList.toggle('active-menu');
+    };
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
+    menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+  };
+  toggleMenu();
+
+  // popup
+  const togglePopUp = () => {
+    const popup = document.querySelector('.popup'),
+          popupContent = document.querySelector('.popup-content'),
+          popupBtn = document.querySelectorAll('.popup-btn'),
+          popUpClose = document.querySelector('.popup-close'),
+          body = document.querySelector('body');
+
+    popupBtn.forEach((elem) => {
+      elem.addEventListener('click', () => {
+        popup.style.display = 'block';
+
+        let width = document.documentElement.offsetWidth;
+        console.log('width: ', width);
+
+        if (width >= 768) {
+          popupContent.style.left = 0 + 'px';
+          popupContent.style.top = 0 + 'px';
+          let flyInterval,
+              count = 0; 
+
+          let flyAnimate = function() {
+            flyInterval = requestAnimationFrame(flyAnimate);
+            count++;
+            if(count < 40){
+              popupContent.style.left = count * 10 + 'px';
+              popupContent.style.top = count * 3 + 'px';
+            } else {
+              cancelAnimationFrame(flyInterval);
+            }
+          };
+          flyInterval = requestAnimationFrame(flyAnimate);
+        }
+        
+      });
+    });    
+
+    popUpClose.addEventListener('click', () => {
+      popup.style.display = 'none';
+    });
+  };
+
+  togglePopUp();
 
 });
