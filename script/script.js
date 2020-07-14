b91f97901af19c6a65c8657532096ba0e65a5f2b
Vitalii <60194255+VetalSh@users.noreply.github.com>
2020-07-14 15:35:13 +0300

    lesson26 part1

--- script/script.js
+++ script/script.js
@@ -368,4 +368,81 @@ window.addEventListener('DOMContentLoaded', function() {
 
   calc(100);
 
+  // send-ajax-form
+
+  const sendForm = () => {
+    const errorMessage = 'Что-то пошло не так...',
+          loadMessage = 'Загрузка...',
+          successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
+
+    const form = document.getElementById('form1'),
+          form2 = document.getElementById('form2');
+
+    const statusMessage = document.createElement('div');
+    // statusMessage.textContent = 'Тут будет сообщение';
+    statusMessage.style.cssText = 'font-size: 2rem;';
+    // form.appendChild(statusMessage);
+
+    form.addEventListener('submit', (event) => {
+            
+      event.preventDefault();
+      form.appendChild(statusMessage);      
+      statusMessage.textContent = loadMessage;
+      const formData = new FormData(form);
+      let body = {};
+      // 1 case
+      // for (let val of formData.entries()) {
+      //   body[val[0]] = val[1];
+      // }
+      // 2 case
+      formData.forEach((val, key) => {
+        body[key] = val;
+      });
+      postData(body,
+         () => {
+        statusMessage.textContent = successMessage;
+        clearForm1();
+      }, (error) => {
+        console.error(error);
+        statusMessage.textContent = errorMessage;
+        clearForm1();
+      });
+    });
+
+    // Функция очистки формы1
+    const clearForm1 = () => {      
+      const form1Name = document.getElementById('form1-name'),
+            form1Email = document.getElementById('form1-email'),
+            form1Phone = document.getElementById('form1-phone');
+
+      form1Name.value = '';
+      form1Email.value = '';
+      form1Phone.value = '';
+    };
+
+    const postData = (body, outputData, errorData) => {
+      const request = new XMLHttpRequest();
+      request.addEventListener('readystatechange', () => {
+        if (request.readyState !== 4) {
+          return;
+        }
+        if (request.status === 200) {
+          outputData();
+        } else {
+          errorData(request.status);
+        }
+      });
+      
+      request.open('POST', './server.php');
+      // request.setRequestHeader('Content-Type', 'multipart/form-data');
+      request.setRequestHeader('Content-Type', 'application/json');
+      
+      // request.send(formData);
+      request.send(JSON.stringify(body));
+    };
+
+  };
+
+  sendForm();
+
 });
